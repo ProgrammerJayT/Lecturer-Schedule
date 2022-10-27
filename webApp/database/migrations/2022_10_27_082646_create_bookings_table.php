2ab0314bf3cb21fdf3e649efe2e1041dea159e75
Programmer JayT <theanthem8@gmail.com>
2022-10-27 10:33:27 +0200

    Created booking table migration

---
+++ webApp/database/migrations/2022_10_27_082646_create_bookings_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bookings', function (Blueprint $table) {
+            $table->id();
+            $table->integer('lecturer_id');
+            $table->integer('student_id');
+            $table->date('booking_date');
+            $table->string('booking_time');
+            $table->string('booking_type');
+            $table->string('booking_status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bookings');
+    }
+};
